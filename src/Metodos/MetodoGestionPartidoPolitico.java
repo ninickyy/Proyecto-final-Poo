1f256c04ab0d25fe44575d9fd534b002022a7806
20248056 <20248056@aloe.ulima.edu.pe>
2026-02-15 19:37:39 -0500

    c

--- src/Metodos/MetodoGestionPartidoPolitico.java
+++ src/Metodos/MetodoGestionPartidoPolitico.java
@@ -1,9 +1,14 @@
-public class GestorPartidoPolitico {
+public class MetodoGestionPartidoPolitico {
     //Atributos
     private partidoPolitico[] partidos;
     private int nroPartidos;
 
     //Constructor sin parametros
+    public MetodoGestionPartidoPolitico(){
+        partidos = partidoPolitico[100];
+        contador = 0;
+    }
 
     
+
 }
