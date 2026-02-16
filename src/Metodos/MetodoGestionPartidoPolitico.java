a947334a1fd39c669b571d736030deea2cc9eed7
20248056 <20248056@aloe.ulima.edu.pe>
2026-02-15 21:17:09 -0500

    cc

--- src/Metodos/MetodoGestionPartidoPolitico.java
+++ src/Metodos/MetodoGestionPartidoPolitico.java
@@ -16,7 +16,7 @@ public class MetodoGestionPartidoPolitico {
             partidos[contador] = partido;
             contador++;
         }else{
-            System.out.println("No es posible agregar más partidos.")
+            System.out.println("No es posible agregar más partidos.");
         }
     }
 
@@ -24,7 +24,30 @@ public class MetodoGestionPartidoPolitico {
 
     public void modificar(int posicion, String nombrePartido, String sigla, String logo, String representanteLegal){
         //Que este dentro del rango
-        if (posicion)
+        if (posicion>=0&&posicion<contador){
+            partidoPolitico[posicion].setNombrePartido(nombrePartido);
+            partidoPolitico[posicion].setSigla(sigla);
+            partidoPolitico[posicion].setLogo(logo);
+            partidoPolitico[posicion].setRepresentanteLegal(representanteLegal);
+
+        }else{
+            System.out.println("Ingrese una posicion valida.");
+        }
+    }
+
+    //Eliminar partido
+
+
+    public void eliminar(int posicion){
+        //Que este dentro del rango
+        if (posicion>=0&&posicion<contador){
+            for (int i = posicion; i<posicion-1;i++){
+                
+                partido[i]=partido[i+1];
+
+            }
+            contador--;
+        }
     }
 
 }
