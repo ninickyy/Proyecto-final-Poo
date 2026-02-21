25d62567cf44560b92ff3b481fc9d1fc4db7536e
20242286-spec <20242286@aloe.ulima.edu.pe>
2026-02-20 20:54:24 -0500

    Update PruebaUsuario.java

--- src/Autenticación de operadores/PruebaUsuario.java
+++ src/Autenticación de operadores/PruebaUsuario.java
@@ -1,11 +1,1 @@
-package Metodos;
-import java.util.Scanner;
-import modelo.Usuario;
-public class PruebaUsuario {
-    public static void main (String [] args){
-        Usuario user1 = new Usuario();
-        System.out.println("Información del usuario: ");
-        user1.leer();
-        System.out.println("Gracias por registrar sus datos");
-    }
-}
+¿
