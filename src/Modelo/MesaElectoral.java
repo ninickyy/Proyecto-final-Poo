c179527231e4ecf7ac52c57383ea674504ff38a6
Freddyaxl <axelfreddy23@gmail.com>
2026-02-22 20:10:20 -0500

    MesaElectoral.java

--- src/Modelo/MesaElectoral.java
+++ src/Modelo/MesaElectoral.java
@@ -1,37 +1,74 @@
 package Modelo;
 public class MesaElectoral {
     //Atributos
-    private String codigo;
+    private String codigoMesa;
+    private String lugar; 
     private MiembroDeMesa[] miembros;
-    private int totalMiembros;
+    private int nroVotantesRegistrados;
+    private int nroVotantesEfectivos;
+    
     //Constructor
-
-    public MesaElectoral(String codigo) {
-        this.codigo = codigo;
+    public MesaElectoral() {
+        codigoMesa = "";
+        lugar = "";
         miembros = new MiembroDeMesa[3];
-        totalMiembros = 0;
+        nroVotantesRegistrados = 0;
+        nroVotantesEfectivos =0;
     }
-    //Metodo
-    public void agregarMiembro(MiembroDeMesa m) {
-        if (totalMiembros < miembros.length) {
-        miembros[totalMiembros] = m;
-        totalMiembros++;
-        }
-    }
-    //Getters
 
-    public String getCodigo() {
-        return codigo;
+    public MesaElectoral(String codigoMesa, String lugar, MiembroDeMesa[] miembros, int nroVotantesRegistrados, int nroVotantesEfectivos) {
+        this.codigoMesa = codigoMesa;
+        this.lugar = lugar;
+        this.miembros = miembros;
+        this.nroVotantesRegistrados = nroVotantesRegistrados;
+        this.nroVotantesEfectivos = nroVotantesEfectivos;
+    }
+
+    public String getCodigoMesa() {
+        return codigoMesa;
+    }
+
+    public void setCodigoMesa(String codigoMesa) {
+        this.codigoMesa = codigoMesa;
+    }
+
+    public String getLugar() {
+        return lugar;
+    }
+
+    public void setLugar(String lugar) {
+        this.lugar = lugar;
     }
 
     public MiembroDeMesa[] getMiembros() {
         return miembros;
     }
 
-    public int getTotalMiembros() {
-        return totalMiembros;
+    public void setMiembros(MiembroDeMesa[] miembros) {
+        this.miembros = miembros;
     }
-    
-    
-    
+
+    public int getNroVotantesRegistrados() {
+        return nroVotantesRegistrados;
+    }
+
+    public void setNroVotantesRegistrados(int nroVotantesRegistrados) {
+        this.nroVotantesRegistrados = nroVotantesRegistrados;
+    }
+
+    public int getNroVotantesEfectivos() {
+        return nroVotantesEfectivos;
+    }
+
+    public void setNroVotantesEfectivos(int nroVotantesEfectivos) {
+        this.nroVotantesEfectivos = nroVotantesEfectivos;
+    }
+   public void verInfo(){
+        System.out.println("Codigo de mesa: "+codigoMesa);
+        System.out.println("lugar: "+lugar);
+        System.out.println("Total de votantes Registrados: "+nroVotantesRegistrados);
+        System.out.println("total de votantes Efectivos: "+nroVotantesEfectivos);
+   }
+   
 }
+
