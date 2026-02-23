1d7399bd163023cd1d12c88dc00c9229d15dee7a
20242286-spec <20242286@aloe.ulima.edu.pe>
2026-02-22 23:02:22 -0500

    Update Usuario.java

--- codigofinal/src/Clase/Usuario.java
+++ codigofinal/src/Clase/Usuario.java
@@ -5,15 +5,42 @@ public class Usuario {
     private String usuario;
     private String clave;
 
+    public Usuario() {
+        this.usuario = "";
+        this.clave = "";
+    }
+
     public Usuario(String usuario, String clave) {
         this.usuario = usuario;
         this.clave = clave;
     }
 
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+
+    public String getClave() {
+        return clave;
+    }
+
+    public void setClave(String clave) {
+        this.clave = clave;
+    }
+
     public boolean IniciarSesion(String user, String pass) {
-        if (this.usuario.equals(user) && this.clave.equals(pass)) {
+
+        boolean usuarioCorrecto = this.usuario.equals(user);
+        boolean claveCorrecta = this.clave.equals(pass);
+
+        if (usuarioCorrecto && claveCorrecta) {
             return true;
         }
+
         return false;
     }
+
 }
