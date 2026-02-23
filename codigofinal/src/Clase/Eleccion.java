a85c2588cce7d02a815e63b703158f8e9fd3dafc
Freddyaxl <axelfreddy23@gmail.com>
2026-02-22 20:33:21 -0500

    Add files via upload

---
+++ codigofinal/src/Clase/Eleccion.java
@@ -0,0 +1,47 @@
+package Modelo;
+public class Eleccion {
+    //Atributos
+    private String fecha;
+    private String tipo;
+    private Candidato[] candidatos;
+    //Constructores
+    public Eleccion() {
+        fecha = "";
+        tipo = "";
+        candidatos = null;
+    }
+    public Eleccion(String fecha, String tipo, Candidato[] candidatos) {
+        this.fecha = fecha;
+        this.tipo = tipo;
+        this.candidatos = candidatos;
+    }
+    //Getters and Setters
+    public String getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
+    public Candidato[] getCandidatos() {
+        return candidatos;
+    }
+
+    public void setCandidatos(Candidato[] candidatos) {
+        this.candidatos = candidatos;
+    }
+    public void verInfo(){
+        System.out.println("Fecha de eleccion: "+fecha);
+        System.out.println("tipo de eleccion: "+tipo);
+        System.out.println("Candidatos: "+candidatos);
+    }
+}
