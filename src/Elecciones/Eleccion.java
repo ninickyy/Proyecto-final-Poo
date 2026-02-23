199e4936d68022fadf8d7f163cbc9172008c5f02
Freddyaxl <axelfreddy23@gmail.com>
2026-02-22 19:46:37 -0500

    Eleccion.java

--- src/Elecciones/Eleccion.java
+++ src/Elecciones/Eleccion.java
@@ -3,39 +3,45 @@ public class Eleccion {
     //Atributos
     private String fecha;
     private String tipo;
-    private MesaElectoral[] mesas;
-    private int totalMesas;
-    
-    //Constructor
-
-    public Eleccion(String fecha, String tipo) {
+    private Candidato[] candidatos;
+    //Constructores
+    public Eleccion() {
+        fecha = "";
+        tipo = "";
+        candidatos = null;
+    }
+    public Eleccion(String fecha, String tipo, Candidato[] candidatos) {
         this.fecha = fecha;
         this.tipo = tipo;
-        mesas = new MesaElectoral[20];
-        totalMesas = 0;
+        this.candidatos = candidatos;
     }
-    //Metodo
-    public void agregarMesa(MesaElectoral m) {
-        mesas[totalMesas] = m;
-        totalMesas++;
-    }
-    //Getters
-
+    //Getters and Setters
     public String getFecha() {
         return fecha;
+    }
+
+    public void setFecha(String fecha) {
+        this.fecha = fecha;
     }
 
     public String getTipo() {
         return tipo;
     }
 
-    public MesaElectoral[] getMesas() {
-        return mesas;
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
     }
 
-    public int getTotalMesas() {
-        return totalMesas;
+    public Candidato[] getCandidatos() {
+        return candidatos;
     }
-   
-    
+
+    public void setCandidatos(Candidato[] candidatos) {
+        this.candidatos = candidatos;
+    }
+    public void verInfo(){
+        System.out.println("Fecha de eleccion: "+fecha);
+        System.out.println("tipo de eleccion: "+tipo);
+        System.out.println("Candidatos: "+candidatos);
+    }
 }
