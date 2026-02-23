e34b00ef0fa2d077a1848f0454e995320913eebc
- nicky <nicolefesar1@gmail.com>
2026-02-22 20:37:58 -0500

    Add files via upload
    .

---
+++ codigofinal/src/Gestion/GestionActaElectoral.java
@@ -0,0 +1,38 @@
+package método;
+import modelo .*;
+public class GestionActaElectoral {
+    private ActaElectoral[] actas = new ActaElectoral[25];
+    private int totalActas = 0; 
+    
+    //Métodos
+    public void registrarActaElectoral(ActaElectoral a){
+        //en base al contador se van agregando las actas        
+        actas[totalActas]=a;
+        //por eso va aumentando 
+        totalActas++;
+    }
+    
+    public void modificarActaElectoral(int nroActa, String nuevoTitulo, String nuevaFecha){
+        for (int i = 0; i < totalActas; i++) {
+            if(actas[i].getNumeroActa() == nroActa){
+                actas[i].getTitulo().equals(nuevoTitulo);
+                actas[i].getFecha().equals(nuevaFecha);
+                System.out.println("Se modifico el acta");
+                return;
+            }
+        }
+        System.out.println("Acta no modificada");
+    }
+    
+    public void eliminarActaElectoral(int nroActa){
+        for (int i = 0; i < totalActas; i++) {
+            if(actas[i].getNumeroActa() == nroActa){
+                actas[i] = actas[totalActas-1];
+                totalActas--;
+                System.out.println("Se elimino el acta");
+                return;
+            }            
+        }
+        System.out.println("Acta no eliminada");
+    }
+}
