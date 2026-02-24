2fa3527145094ec719c85d115f6726ba4a0c8c9f
Freddyaxl <axelfreddy23@gmail.com>
2026-02-24 05:32:29 -0500

    Usuario.java

--- codigofinal/src/Clase/Usuario.java
+++ codigofinal/src/Clase/Usuario.java
@@ -5,42 +5,15 @@ public class Usuario {
     private String usuario;
     private String clave;
 
-    public Usuario() {
-        this.usuario = "";
-        this.clave = "";
-    }
-
     public Usuario(String usuario, String clave) {
         this.usuario = usuario;
         this.clave = clave;
     }
 
-    public String getUsuario() {
-        return usuario;
-    }
-
-    public void setUsuario(String usuario) {
-        this.usuario = usuario;
-    }
-
-    public String getClave() {
-        return clave;
-    }
-
-    public void setClave(String clave) {
-        this.clave = clave;
-    }
-
-    public boolean IniciarSesion(String user, String pass) {
-
-        boolean usuarioCorrecto = this.usuario.equals(user);
-        boolean claveCorrecta = this.clave.equals(pass);
-
-        if (usuarioCorrecto && claveCorrecta) {
+    public boolean iniciarSesion(String user, String pass) {
+        if (this.usuario.equals(user) && this.clave.equals(pass)) {
             return true;
         }
-
         return false;
     }
-
 }
