2eda28552f25f614ec229bc91f306a23bb56526b
Freddyaxl <axelfreddy23@gmail.com>
2026-02-24 05:24:59 -0500

    GestionEleccion.java

--- codigofinal/src/Gestion/GestionEleccion.java
+++ codigofinal/src/Gestion/GestionEleccion.java
@@ -19,7 +19,6 @@ public class GestionEleccion {
         this.total = total;
     }
     //Getters and Setters
-
     public Eleccion[] getElecciones() {
         return elecciones;
     }
@@ -38,33 +37,58 @@ public class GestionEleccion {
     
     //Registrar eleccion
     public void registrarEleccion(String fecha, String tipo, Candidato[] candidatos) {
+        //se verifica que aun haya espacio en el arreglo
         if (total < elecciones.length) {
-            Eleccion e = new Eleccion(fecha, tipo, candidatos);
-            elecciones[total] = e;
+            Eleccion e = new Eleccion(fecha, tipo, candidatos); //crea nueva eleccion
+            elecciones[total] = e; //guarda la eleccion 
             total++;
         } else {
             System.out.println("No hay espacio para registrar mas elecciones");
         }
     }
     //Modificacion de eleccion
-    public void modificarEleccion(int posicion, String nuevaFecha, String nuevoTipo) {
-        if (posicion >= 0 && posicion < total) {
-            elecciones[posicion].setFecha(nuevaFecha);
-            elecciones[posicion].setTipo(nuevoTipo);
+    public void modificarEleccion(String fecha, String tipo) {
+
+    for (int i = 0; i < total; i++) {
+        //se busca la eleccion por su fecha
+        if (elecciones[i].getFecha().equals(fecha)) {
+            //se actualiza los datos
+            elecciones[i].setFecha(fecha);
+            elecciones[i].setTipo(tipo);
+            System.out.println("Elección modificada");
+            return;
         }
     }
-    
+    System.out.println("Elección no encontrada");
+    }    
+   
     //Eliminar eleccion
-    public void eliminarEleccion(int pos) {
-        if (pos >= 0 && pos < total) {
-            for (int i = pos; i < total - 1; i++) {
-                elecciones[i] = elecciones[i + 1];
+    public void eliminarEleccion(String fecha, String tipo) {
+
+    for (int i = 0; i < total; i++) {
+        //se compara fecha y tipo
+        if (elecciones[i].getFecha().equals(fecha)
+            && elecciones[i].getTipo().equals(tipo)) {
+            //retrocede una posicion atras
+            for (int j = i; j < total - 1; j++) {
+                elecciones[j] = elecciones[j + 1];
             }
+            //ultima posicion eliminada
             elecciones[total - 1] = null;
             total--;
+            return;
+            }
         }
-    
     }
- }
+    public String generarInforme() {
+    String mensaje = "Elecciones:\n\n";
+    for (int i = 0; i < total; i++) {
+        mensaje += "Fecha: " + elecciones[i].getFecha()
+                +  "\nTipo: " + elecciones[i].getTipo()+ 
+                "\n\n";
+    }
+    return mensaje;
+    }
+}
 
 
